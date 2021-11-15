10fd1aec33f00ed37a8f02a3a39325aeaa1e1b70
Artem <334947@niuitmo.ru>
2021-11-15 23:22:22 +0300

    Homework 9 v 0.1.1 Added marker "code"

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
         StringBuilder s = new StringBuilder();
         Paragraph paragraph1 = new Paragraph(List.of(
-                new Text("1235")
+                new Code(List.of(
+                        new Text("1235")
+                ))
         ));
         paragraph1.toHtml(s);
         System.out.println(s);
